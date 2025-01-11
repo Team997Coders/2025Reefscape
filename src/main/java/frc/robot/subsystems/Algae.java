3cc5e7e300f4a1c581fc426b4a24be6b9f841d9e
unknown <coralineandalbus@gmail.com>
2025-01-11 11:57:17 -0800

    redid the algae thingy so now it has loggers and a periodic function

--- src/main/java/frc/robot/subsystems/Algae.java
+++ src/main/java/frc/robot/subsystems/Algae.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
@@ -21,12 +23,24 @@ public class Algae extends SubsystemBase{
         spinnyMotorConfig.smartCurrentLimit(Constants.Algae.spinnyMotorConfig);
 
         proximitySensor = new DigitalInput(Constants.Algae.proximitySensorID);
+    
+
+    @Override
+    public void periodic() {
+        loggers();
     }
 
     public void SpinMotor(double speed){
         spinnyMotor.set(speed);
     }
     
+    public boolean getProximitySensor() {
+        return proximitySensor.get();
+    }
+
+    public void loggers() {
+        SmartDashboard.putBoolean("algae proximity sensor", getProximitySensor());
+    }
 }
 
 
