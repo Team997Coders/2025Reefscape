d41de96891e019a9eb0a634625b2d9473b578af6
unknown <coralineandalbus@gmail.com>
2025-02-08 11:49:49 -0800

    fixed weird merge thing with LEDs branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -139,17 +139,4 @@ public class Robot extends TimedRobot {
   public void simulationPeriodic() {
   }
 
-     // PWM port 9
-    // Must be a PWM header, not MXP or DIO
-    m_led = new AddressableLED(9);
-
-    // Reuse buffer
-    // Default to a length of 60, start empty output
-    // Length is expensive to set, so only set it once, then just update data
-    m_ledBuffer = new AddressableLEDBuffer(60);
-    m_led.setLength(m_ledBuffer.getLength());
-
-    // Set the data
-    m_led.setData(m_ledBuffer);
-    m_led.start();
 }
