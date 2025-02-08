03b95984fbf38ba2bc7c151e751d6a5366f5dbdc
KiraA-R <kira.romanowski@gmail.com>
2025-02-08 12:38:16 -0800

    fixed error in robotcontainer ( two commands set to the A button)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -269,12 +269,14 @@ public class RobotContainer {
     // c_driveStick.povUp().onTrue(Commands.runOnce(gyro::reset));
     Command goToTag = new goToTag(drivebase, frontCamera, 0.0);
     Command stop = new stop(goToTag);
-    //JoystickButton button_a = new JoystickButton(driveStick, 1);
-    // button_a.onTrue(goToTag).onFalse(stop);
+    //// JoystickButton button_a = new JoystickButton(driveStick, 1);
+    // // button_a.onTrue(goToTag).onFalse(stop);
     m_driverController.a().whileTrue(m_algae.AlgaeIntake(Constants.Algae.motorSpin));
     m_driverController.b().whileTrue(m_algae.AlgaeOuttake(Constants.Algae.motorSpin));
     m_driverController.x().whileTrue(m_coral.manualMoveCoralMotorsIntake());
     m_driverController.y().whileTrue(m_coral.manualMoveCoralMotorsOutake());
+
+
 
     c_driveStick.leftBumper().toggleOnTrue(new ElevatorManualControl(elevator, ()->c_driveStick.povUp().getAsBoolean(),
         ()->c_driveStick.povDown().getAsBoolean()));
