be88f925c82ef30ce12b740f224cc39d26b6b667
unknown <coralineandalbus@gmail.com>
2025-02-18 19:18:42 -0800

    updated LEDs to create gradient patterns

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -1,49 +1,72 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Centimeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.Second;
+
 import java.util.Optional;
 
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.LEDPattern.GradientType;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class LED extends SubsystemBase{
 
     private final LEDPattern red;
     private final AddressableLEDBuffer m_buffer;
-    private final AddressableLEDBufferView m_left;
-    private final AddressableLEDBufferView m_right;
     private final AddressableLED m_led;
     private final LEDPattern blue;
     private final LEDPattern purple;
+    private final LEDPattern redPattern;
+    private final LEDPattern bluePattern;
 
+
+//     // Create an LED pattern that displays a red-to-blue gradient, then scroll at one quarter of the LED strip's length per second.
+// // For a half-meter length of a 120 LED-per-meter strip, this is equivalent to scrolling at 12.5 centimeters per second.
+// Distance ledSpacing = Meters.of(1 / 120.0);
+// LEDPattern base = LEDPattern.discontinuousGradient(Color.kRed, Color.kBlue);
+// LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(25));
+// LEDPattern absolute = base.scrollAtAbsoluteSpeed(Centimeters.per(Second).of(12.5), ledSpacing);
+
+// // Apply the LED pattern to the data buffer
+// pattern.applyTo(m_ledBuffer);
+
+// // Write the data to the LED strip
+// m_led.setData(m_ledBuffer);
     public LED(){
         
         m_buffer = new AddressableLEDBuffer(120);
-        m_left = m_buffer.createView(0, 59);
-        m_right = m_buffer.createView(60, 119).reversed();
         m_led = new AddressableLED(0);
-        // Create an LED pattern that sets the entire strip to solid red
-       red = LEDPattern.solid(Color.kRed);
-       blue = LEDPattern.solid(Color.kBlue);
-       purple = LEDPattern.solid(Color.kPlum);
-       
 
+        red = LEDPattern.gradient(GradientType.kContinuous, Color.kFirstRed, Color.kOrangeRed);
+        blue = LEDPattern.gradient(GradientType.kContinuous, Color.kFirstBlue, Color.kLightBlue);
+        purple = LEDPattern.solid(Color.kPlum);
 
+        redPattern = red.scrollAtRelativeSpeed(Percent.per(Second).of(25));
+        bluePattern = blue.scrollAtRelativeSpeed(Percent.per(Second).of(25));
     }
+
+     
+
+
         public void SetColor() {
             Optional<Alliance> ally = DriverStation.getAlliance();
             if (ally.isPresent()) {
                 if (ally.get() == Alliance.Red) {
                     // Apply the LED pattern to the data buffer
-                    red.applyTo(m_buffer);
+                    redPattern.applyTo(m_buffer);
                 }
                 if (ally.get() == Alliance.Blue) {
-                    blue.applyTo(m_buffer);
+                    bluePattern.applyTo(m_buffer);
                 }
             }
              else {
