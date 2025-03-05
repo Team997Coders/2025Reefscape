d6cd0dfe7701795df0c43b8afcbbb7cec73b2d94
unknown <ismaelsuring@gmail.com>
2025-03-04 19:35:23 -0800

    testing the goToTag command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -292,6 +292,8 @@ public class RobotContainer {
     // c_driveStick.povUp().onTrue(elevator.stateUp());
     // c_driveStick.povDown().onTrue(elevator.stateDown());
 
+    c_driveStick.x().onTrue(new goToLocation(drivebase, new Pose2d(1.13,1.05, new Rotation2d(.9425))));
+
     c_driveStick.povUp().whileTrue(elevator.manualUp());
     c_driveStick.povDown().whileTrue(elevator.manualDown());
 
