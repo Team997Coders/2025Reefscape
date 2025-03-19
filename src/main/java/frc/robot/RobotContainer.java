792ffa5a4fd2332083a4c2c2ed1d33fb57837d66
unknown <ismaelsuring@gmail.com>
2025-03-18 18:28:38 -0700

    transform for goTo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import com.reduxrobotics.canand.CanandEventLoop;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
@@ -295,7 +296,7 @@ public class RobotContainer {
    
     //LEFT REEF 0: (3.175+0.195, 4.191+.0254, rotation 0
     //LEFT REEF 5: new Pose2d(3.69 + 0.03 + 0.19 * Math.cos(1.047),2.971 + 0.19 * Math.sin(1.047), new Rotation2d(1.047)
-    c_driveStick.x().whileTrue(new goToLocation(drivebase, new Pose2d(3.69 + 0.03 + 0.19 * Math.cos(1.047),2.971 + 0.19 * Math.sin(1.047), new Rotation2d(1.047))));
+    c_driveStick.x().whileTrue(new goToLocation(drivebase,  drivebase.getPose().transformBy(new Transform2d(1, 1, new Rotation2d()))));
 
     c_driveStick.povUp().whileTrue(elevator.manualUp());
     c_driveStick.povDown().whileTrue(elevator.manualDown());
